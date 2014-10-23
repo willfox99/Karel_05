1eca1cff459167e55ee11fb11aa3569a552ee07c
Kent Collins <kent.r.collins@gmail.com>
2014-10-23 20:54:52 +0100

    Update and rename Lab00.java to Lab04.java

--- Lab04.java
+++ Lab04.java
@@ -9,7 +9,7 @@
 import edu.fcps.karel2.Display;
 import edu.fcps.karel2.Robot;
 
-public class Lab00 {
+public class Lab04 {
 
      public static void main(String[] args) {
              Display.openWorld("maps/first.map");
