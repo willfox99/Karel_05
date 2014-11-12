a0b5ccf8ce9e834ebf9d1ac310ec1795c266b9bf
Kent Collins <kent.r.collins@gmail.com>
2014-11-12 07:14:48 +0000

    updated

--- Lab05.java
+++ Lab05.java
@@ -10,8 +10,13 @@ import edu.fcps.karel2.Display;
 import edu.fcps.karel2.Robot;
 
 public class Lab05 {
+	
+	//TODO define a class method, runTheRace, that will accept a Racer as argument
+	//TODO implement the runTheRace method so that it shuttles the Racer as needed
 
      public static void main(String[] args) {
-          // TODO Your code goes here
+          Display.openWorld("maps/shuttle.map");
+          Display.setSize(10, 10);
+          //TODO Construct three Racers and pass each to "runTheRace", in sequence
      }
 }
--- Racer.java
+++ Racer.java
@@ -1,12 +1,50 @@
+import edu.fcps.karel2.Display;
+
 /**
-*  <Describe that the Racer class does>
+*  <Describe what the Racer class does>
 *
-*  @author <your name here, no brackets>
-*  @version <version or date, no brackets>
+* @author <Your Name here>  // replace <...> with your name
+* @version <date/of/completion> // replace <...> with the date
 */
 
 public class Racer extends Athlete {
 
-// consult the Karel API to determine what constructors and methods this class should implement
+	public Racer(int y) {
+		super(1, y, Display.EAST, Display.INFINITY);
+	}
+	
+	public void jumpRight() {
+		// Pseudocode: tL, m, tR, m, tR, m, tL
+	}
+	
+	public void jumpLeft() {
+		
+	}
+	
+	public void sprint(int n) {
+		for (int k = 1; k<=n; k++) {
+			move();
+		}
+	}
+	
+	public void put(int n) {
+		//TODO Place n beepers
+	}
+	
+	public void pick(int n) {
+		//TODO Pick up n beepers
+	}
+	
+	/**
+	 * Racers shuttle by moving forward, jumping any hurdles along the way, and collecting
+	 * the required number of beepers.  Racers then return to their starting position and
+	 * deposit the collected beepers.  Finally, Racers face East to complete the shuttle.
+	 * 
+	 * @param spaces
+	 * @param beepers
+	 */
+	public void shuttle(int spaces, int beepers) {
+		//TODO Perform a complete shuttle sequence
+	}
 
 }
